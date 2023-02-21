9cf671a0cc96580825d49f7a15762be90e90976d
ot8le <77287542+ot8le@users.noreply.github.com>
2023-02-21 10:26:01 -0500

    Update trashTheCache.sh

--- trashTheCache.sh
+++ trashTheCache.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 
+# Change to library cache directory and remove cache
 cd ~/Library/Caches/; rm -rf ~/Library/Caches/*
+
+# Prompt user to restart their system
 osascript -e 'tell app "loginwindow" to «event aevtrrst»'
+
+# Clear terminal and output to user
 clear
-echo "System data was removed."
+echo "System data storage section was reset."
 echo "Restart for changes to take effect."
